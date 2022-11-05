2cf3af5dadeabf5d6934944b4ef8c7371e135c6e
James Anh <james.anh184@gmail.com>
2022-11-04 22:43:57 -0700

    added artist detail endpoints

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,5 +1,65 @@
-const ArtistDetails = () => (
-  <div>ArtistDetails</div>
-);
+import { useParams } from "react-router-dom";
+import { useSelector} from "react-redux";
+import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
+import { useGetArtistDetailsQuery} from "../redux/services/shazamCore";
+const ArtistDetails = () => {
+    const dispatch = useDispatch();
+    const {songid, id: artistId} = useParams();
+    const {activeSong, isPlaying} = useSelector((state) => state.player)
+    //
+
+    // fetch data from redux
+    // Lyric can be found in songData under sections
+    // SongData.sections[1].text
+    
+    const {data: songData, isFetching: isFetchingSongDetails} = useGetSongDetailsQuery({songid})
+    // related Song 
+    const {data, isFetching: isFetchingRelatedSongs, error} = useGetSongsRelatedQuery({songid});
+    console.log(songData)
+    // Handle Pause
+    const handlePauseClick = () => {
+        dispatch(playPause(false))
+      };
+      // play Handle
+      const handlePlayClick = (song, i) => {
+        dispatch(setActiveSong({song,data,i}));
+        dispatch(playPause(true));
+      };
+    
+    // Loading Error component while fetching
+    if(isFetchingSongDetails || isFetchingRelatedSongs) return <Loader />
+    // handle error
+    if(error) return <Error />
+    return (
+        <div className="flex flex-col ">
+            <DetailsHeader 
+                artistId={artistId}
+                songData={songData}
+            />
+            <div className="mb-10">
+                <h2 className="text-white text-3xl font-bold">
+                    Lyric:
+                </h2>
+                <div className="mt-5 ">
+                    {songData?.sections[1].type === "LYRICS" ? songData?.sections[1].text.map((Line, i) => (
+                        <p className="text-white text-base my-1">{Line}</p>
+                    )) : <p>Sorry, Lyric not found!</p> }
+                </div>
+            </div>
+            <RelatedSongs
+            data={data}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            handlePauseClick={handlePauseClick}
+            handlePlayClick={handlePlayClick}
+            />
+        </div>
+    )
+
+
+
+
+}
 
 export default ArtistDetails;
+
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -26,8 +26,9 @@ export const shazamCoreApi = createApi({
     endpoints: (builder) => ({
         getTopCharts: builder.query({query: () => '/charts/world'}),
         getSongDetails: builder.query({query: ({songid}) => `/tracks/details?track_id=${songid}`}),
-        getSongsRelated: builder.query({query: ({songid}) => `/tracks/related?track_id=${songid}`})
+        getSongsRelated: builder.query({query: ({songid}) => `/tracks/related?track_id=${songid}`}),
+        getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}`})
     }),
 })
 
-export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongsRelatedQuery} = shazamCoreApi 
+export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongsRelatedQuery,useGetArtistDetailsQuery} = shazamCoreApi 
