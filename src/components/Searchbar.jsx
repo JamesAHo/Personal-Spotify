53a11ec0035d5d02f52ea014c2004a2cc89c4123
James Anh <james.anh184@gmail.com>
2022-11-05 22:37:14 -0700

    set up search bar components

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {FiSearch} from 'react-icons/fi'
+
 const Searchbar = () => (
-  <div>Loader</div>
+  <form autoComplete="off" 
+  className="p-2 text-gray-400 focus-within:text-gray-600">
+    <label htmlFor="search-field" className="sr-only">
+        Search all songs
+    </label>
+    <div className="flex flex-row justify-start items-center">
+
+    </div>
+    </form>
 );
 
 export default Searchbar;
