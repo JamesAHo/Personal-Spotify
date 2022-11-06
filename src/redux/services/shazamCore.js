097ffbc68049fa4383ee69e28643faddcb5ada98
James Anh <james.anh184@gmail.com>
2022-11-05 22:14:19 -0700

    Top charts working, resuing Around you component

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -28,6 +28,8 @@ export const shazamCoreApi = createApi({
         getSongDetails: builder.query({query: ({songid}) => `/tracks/details?track_id=${songid}`}),
         getSongsRelated: builder.query({query: ({songid}) => `/tracks/related?track_id=${songid}`}),
         getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}`}),
+        
+        //this endpoints will respond country code data
         getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` })
     }),
 })
