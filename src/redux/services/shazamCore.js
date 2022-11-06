4d34204df021c43f6cf1a718915be7decf7ed75c
James Anh <james.anh184@gmail.com>
2022-11-05 21:23:06 -0700

    added country code endpoints

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -27,8 +27,9 @@ export const shazamCoreApi = createApi({
         getTopCharts: builder.query({query: () => '/charts/world'}),
         getSongDetails: builder.query({query: ({songid}) => `/tracks/details?track_id=${songid}`}),
         getSongsRelated: builder.query({query: ({songid}) => `/tracks/related?track_id=${songid}`}),
-        getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}`})
+        getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}`}),
+        getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code${countryCode}` })
     }),
 })
 
-export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongsRelatedQuery,useGetArtistDetailsQuery} = shazamCoreApi 
+export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongsRelatedQuery,useGetArtistDetailsQuery, useGetSongsByCountryQuery} = shazamCoreApi 
