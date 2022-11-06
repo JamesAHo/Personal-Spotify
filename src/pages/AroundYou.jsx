ef55a64d62a1c56d4ea9fd913c305d6b0e92d5ca
James Anh <james.anh184@gmail.com>
2022-11-05 21:23:49 -0700

    fetch geo.ipify data

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -2,11 +2,26 @@ import {useState,useEffect } from 'react';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 import {Error, Loader, SongCard} from '../components'
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
 
 const AroundYou = () => {
     const [country, setCountry] = useState('');
     const [loading, setLoading] = useState('');
-    const {activeSong, isPlaying} = useSelector((state) => state.player)
+    const {activeSong, isPlaying} = useSelector((state) => state.player);
+
+    // fetch data from redux enpoints
+    const {data, isFetching, error} = useGetSongsByCountryQuery(country);
+    console.log(country);
+
+    useEffect(() => {
+        // at_4U4cYef04oSqCqM2yPN6sLswtUVjv
+        // fetch API
+        axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_4U4cYef04oSqCqM2yPN6sLswtUVjv`)
+        .then((res) => setCountry(res?.data?.location?.country))
+        .catch((err) => console.log(err))
+        .finally(() => setLoading(false));
+
+    },[country])
     return (
         <div>
 
